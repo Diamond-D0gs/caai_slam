e2d0a6d2cf323720b66887888b506812214e409e
Diamond-D0gs <32776854+Diamond-D0gs@users.noreply.github.com>
2026-01-25 18:45:56 -0500

    Groundwork laid

---
+++ include/caai_slam/mapping/local_map.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "caai_slam/core/types.hpp"
+#include "caai_slam/frontend/keyframe.hpp"
+#include "caai_slam/mapping/covisibility_graph.hpp"
+#include "caai_slam/backend/graph_optimizer.hpp"
